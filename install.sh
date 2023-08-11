ba054660c5c9531caf32e0f73c2331bfc0b82fd8
Cristiano Nunes <cfgnunes@gmail.com>
2023-08-10 22:18:32 -0300

    Bugfix

--- install.sh
+++ install.sh
@@ -58,11 +58,11 @@ _main() {
     read -r -p " > Would you like to install all dependencies for the scripts now? [Y/n]" answer
     case "${answer,,}" in
     y | yes | "")
-        echo "> Installing dependencies..."
-        _install_dependencies
+        echo " > Installing dependencies..."
+        _install_dependencies "baobab binutils bzip2 coreutils cups-bsd eog ffmpeg file-roller findimagedupes foremost ghostscript git gpg gzip imagemagick inkscape jdupes jpegoptim lame lhasa libc-bin lzip lzop meld mp3gain mp3val ocrmypdf optipng p7zip-full pandoc perl-base poppler-utils qpdf rhash squashfs-tools tar testdisk unrar xclip xz-utils zip zstd"
         ;;
     *)
-        echo "> Skipping installation of dependencies..."
+        echo " > Skipping installation of dependencies..."
         ;;
     esac
 
@@ -70,189 +70,21 @@ _main() {
 }
 
 _install_dependencies() {
+    local packages=$1
 
     if _command_exists "sudo"; then
         if _command_exists "apt-get"; then
             sudo apt-get update
-            sudo apt-get -y install \
-                sudo apt-get install \
-                baobab \
-                binutils \
-                bzip2 \
-                coreutils \
-                cups-bsd \
-                eog \
-                ffmpeg \
-                file-roller \
-                findimagedupes \
-                foremost \
-                ghostscript \
-                git \
-                gpg \
-                gzip \
-                imagemagick \
-                inkscape \
-                jdupes \
-                jpegoptim \
-                lame \
-                lhasa \
-                libc-bin \
-                lzip \
-                lzop \
-                meld \
-                mp3gain \
-                mp3val \
-                ocrmypdf \
-                optipng \
-                p7zip-full \
-                pandoc \
-                perl-base \
-                poppler-utils \
-                qpdf \
-                rhash \
-                squashfs-tools \
-                tar \
-                testdisk \
-                unrar \
-                xclip \
-                xz-utils \
-                zip \
-                zstd
+            sudo apt-get -y install $packages
         elif _command_exists "pacman"; then
             sudo pacman -Syy
-            sudo pacman --noconfirm -S \
-                baobab \
-                binutils \
-                bzip2 \
-                coreutils \
-                cups-bsd \
-                eog \
-                ffmpeg \
-                file-roller \
-                findimagedupes \
-                foremost \
-                ghostscript \
-                git \
-                gpg \
-                gzip \
-                imagemagick \
-                inkscape \
-                jdupes \
-                jpegoptim \
-                lame \
-                lhasa \
-                libc-bin \
-                lzip \
-                lzop \
-                meld \
-                mp3gain \
-                mp3val \
-                ocrmypdf \
-                optipng \
-                p7zip-full \
-                pandoc \
-                perl-base \
-                poppler-utils \
-                qpdf \
-                rhash \
-                squashfs-tools \
-                tar \
-                testdisk \
-                unrar \
-                xclip \
-                xz-utils \
-                zip \
-                zstd
+            sudo pacman --noconfirm -S $packages
         elif _command_exists "dnf"; then
             sudo dnf check-update
-            sudo dnf -y install \
-                baobab \
-                binutils \
-                bzip2 \
-                coreutils \
-                cups-bsd \
-                eog \
-                ffmpeg \
-                file-roller \
-                findimagedupes \
-                foremost \
-                ghostscript \
-                git \
-                gpg \
-                gzip \
-                imagemagick \
-                inkscape \
-                jdupes \
-                jpegoptim \
-                lame \
-                lhasa \
-                libc-bin \
-                lzip \
-                lzop \
-                meld \
-                mp3gain \
-                mp3val \
-                ocrmypdf \
-                optipng \
-                p7zip-full \
-                pandoc \
-                perl-base \
-                poppler-utils \
-                qpdf \
-                rhash \
-                squashfs-tools \
-                tar \
-                testdisk \
-                unrar \
-                xclip \
-                xz-utils \
-                zip \
-                zstd
+            sudo dnf -y install $packages
         elif _command_exists "yum"; then
             sudo yum check-update
-            sudo yum -y install \
-                baobab \
-                binutils \
-                bzip2 \
-                coreutils \
-                cups-bsd \
-                eog \
-                ffmpeg \
-                file-roller \
-                findimagedupes \
-                foremost \
-                ghostscript \
-                git \
-                gpg \
-                gzip \
-                imagemagick \
-                inkscape \
-                jdupes \
-                jpegoptim \
-                lame \
-                lhasa \
-                libc-bin \
-                lzip \
-                lzop \
-                meld \
-                mp3gain \
-                mp3val \
-                ocrmypdf \
-                optipng \
-                p7zip-full \
-                pandoc \
-                perl-base \
-                poppler-utils \
-                qpdf \
-                rhash \
-                squashfs-tools \
-                tar \
-                testdisk \
-                unrar \
-                xclip \
-                xz-utils \
-                zip \
-                zstd
+            sudo yum -y install $packages
         else
             echo "Error: could not find a package manager!"
             return 1
