cd13f1be10986dd0fa16d3d80e5227bccefa2c03
Cristiano Nunes <cfgnunes@gmail.com>
2023-08-10 23:59:21 -0300

    Refactor in installer

--- install.sh
+++ install.sh
@@ -55,7 +55,212 @@ _main() {
     echo " > Closing the file manager to reload its configurations..."
     eval "$file_manager -q &>/dev/null" || true
 
+    read -r -p " > Would you like to install all dependencies for the scripts now? [Y/n]" answer
+    case "${answer,,}" in
+    y | yes | "")
+        echo "> Installing dependencies..."
+        _install_dependencies
+        ;;
+    *)
+        echo "> Skipping installation of dependencies..."
+        ;;
+    esac
+
     echo "Done!"
+}
+
+_install_dependencies() {
+
+    if _command_exists "sudo"; then
+        if _command_exists "apt-get"; then
+            sudo apt-get update
+            sudo apt-get -y install \
+                sudo apt-get install \
+                baobab \
+                binutils \
+                bzip2 \
+                coreutils \
+                cups-bsd \
+                eog \
+                ffmpeg \
+                file-roller \
+                findimagedupes \
+                foremost \
+                ghostscript \
+                git \
+                gpg \
+                gzip \
+                imagemagick \
+                inkscape \
+                jdupes \
+                jpegoptim \
+                lame \
+                lhasa \
+                libc-bin \
+                lzip \
+                lzop \
+                meld \
+                mp3gain \
+                mp3val \
+                ocrmypdf \
+                optipng \
+                p7zip-full \
+                pandoc \
+                perl-base \
+                poppler-utils \
+                qpdf \
+                rhash \
+                squashfs-tools \
+                tar \
+                testdisk \
+                unrar \
+                xclip \
+                xz-utils \
+                zip \
+                zstd
+        elif _command_exists "pacman"; then
+            sudo pacman -Syy
+            sudo pacman --noconfirm -S \
+                baobab \
+                binutils \
+                bzip2 \
+                coreutils \
+                cups-bsd \
+                eog \
+                ffmpeg \
+                file-roller \
+                findimagedupes \
+                foremost \
+                ghostscript \
+                git \
+                gpg \
+                gzip \
+                imagemagick \
+                inkscape \
+                jdupes \
+                jpegoptim \
+                lame \
+                lhasa \
+                libc-bin \
+                lzip \
+                lzop \
+                meld \
+                mp3gain \
+                mp3val \
+                ocrmypdf \
+                optipng \
+                p7zip-full \
+                pandoc \
+                perl-base \
+                poppler-utils \
+                qpdf \
+                rhash \
+                squashfs-tools \
+                tar \
+                testdisk \
+                unrar \
+                xclip \
+                xz-utils \
+                zip \
+                zstd
+        elif _command_exists "dnf"; then
+            sudo dnf check-update
+            sudo dnf -y install \
+                baobab \
+                binutils \
+                bzip2 \
+                coreutils \
+                cups-bsd \
+                eog \
+                ffmpeg \
+                file-roller \
+                findimagedupes \
+                foremost \
+                ghostscript \
+                git \
+                gpg \
+                gzip \
+                imagemagick \
+                inkscape \
+                jdupes \
+                jpegoptim \
+                lame \
+                lhasa \
+                libc-bin \
+                lzip \
+                lzop \
+                meld \
+                mp3gain \
+                mp3val \
+                ocrmypdf \
+                optipng \
+                p7zip-full \
+                pandoc \
+                perl-base \
+                poppler-utils \
+                qpdf \
+                rhash \
+                squashfs-tools \
+                tar \
+                testdisk \
+                unrar \
+                xclip \
+                xz-utils \
+                zip \
+                zstd
+        elif _command_exists "yum"; then
+            sudo yum check-update
+            sudo yum -y install \
+                baobab \
+                binutils \
+                bzip2 \
+                coreutils \
+                cups-bsd \
+                eog \
+                ffmpeg \
+                file-roller \
+                findimagedupes \
+                foremost \
+                ghostscript \
+                git \
+                gpg \
+                gzip \
+                imagemagick \
+                inkscape \
+                jdupes \
+                jpegoptim \
+                lame \
+                lhasa \
+                libc-bin \
+                lzip \
+                lzop \
+                meld \
+                mp3gain \
+                mp3val \
+                ocrmypdf \
+                optipng \
+                p7zip-full \
+                pandoc \
+                perl-base \
+                poppler-utils \
+                qpdf \
+                rhash \
+                squashfs-tools \
+                tar \
+                testdisk \
+                unrar \
+                xclip \
+                xz-utils \
+                zip \
+                zstd
+        else
+            echo "Error: could not find a package manager!"
+            return 1
+        fi
+    else
+        echo "Error: could not run the installer as administrator!"
+        return 1
+    fi
 }
 
 _main "$@"
