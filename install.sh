7a79cfb618d8290869d43a66e54ebfa3be4a62be
Cristiano Nunes <cfgnunes@gmail.com>
2023-08-11 00:38:37 -0300

    Bugfix

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ _main() {
 }
 
 _install_dependencies() {
-    local common_names="bzip2 foremost ghostscript gzip jpegoptim lame lzip lzop optipng pandoc perl-base qpdf rhash squashfs-tools tar testdisk unrar xclip zip zstd"
+    local common_names="bzip2 foremost ghostscript gzip jpegoptim lame lzip lzop optipng pandoc perl-base qpdf rhash squashfs-tools tar testdisk unrar wget xclip zip zstd"
 
     if _command_exists "sudo"; then
         if _command_exists "apt-get"; then
