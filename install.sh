3fd90a05bb90e3c40eb71e343b97464ea04fbb5b
Cristiano Nunes <cfgnunes@gmail.com>
2024-01-19 21:30:58 -0300

    Installer with menu options

--- install.sh
+++ install.sh
@@ -7,86 +7,34 @@
 set -eu
 
 _main() {
-    echo "Starting the installation..."
-    local accels_dir=""
-    local file_manager=""
-    local install_dir=""
-    local tmp_install_dir=""
-    local preserve_previous_scripts=true
+    local menu_options=""
+    local menu_exit=""
 
-    # Get the default file manager
-    if _command_exists "nautilus"; then
-        install_dir="$HOME/.local/share/nautilus/scripts"
-        accels_dir="$HOME/.config/nautilus"
-        file_manager="nautilus"
-    elif _command_exists "nemo"; then
-        install_dir="$HOME/.local/share/nemo/scripts"
-        file_manager="nemo"
-    elif _command_exists "caja"; then
-        install_dir="$HOME/.config/caja/scripts"
-        file_manager="caja"
-    else
-        echo "Error: could not find any compatible file managers!"
+    # Show the main menu
+    menu_options=$(whiptail --title "Nautilus Scripts Installer" --separate-output --checklist \
+        "Installer options:" 10 68 4 \
+        "1" "Install basic package dependencies" ON \
+        "2" "Install the file 'scripts-accels'" ON \
+        "3" "Preserve the previous scripts" OFF \
+        "4" "Close the file manager to reload its configurations" ON \
+        3>&1 1>&2 2>&3)
+    menu_exit=$?
+
+    # Check if the user pressed Cancel or Esc no the menu
+    if [[ $menu_exit != 0 ]]; then
+        echo "User cancelled."
         exit 1
     fi
 
-    # Check for previous scripts
-    if [[ -d "$install_dir" ]]; then
+    echo "Starting the installation..."
 
-        # Ask if the user wants to preserve the previous scripts
-        read -r -p " > Would you like to preserve the previous scripts? [y/N]" answer
-        case "${answer,,}" in
-        n | no | "")
-            preserve_previous_scripts=false
-            ;;
-        esac
-
-        # 'Preserve' or 'Remove' previous scripts
-        if $preserve_previous_scripts; then
-            echo " > Preserving previous scripts to a temporary directory..."
-            tmp_install_dir=$(mktemp -d)
-            mv "$install_dir"/* "$tmp_install_dir" || true
-        else
-            echo " > Removing previous scripts..."
-            rm -rf "$install_dir"
-        fi
+    # Install basic package dependencies
+    if [[ "$menu_options" == *1* ]]; then
+        _install_dependencies
     fi
 
     # Install the scripts
-    echo " > Installing new scripts..."
-    mkdir --parents "$install_dir"
-    cp -r ./* "$install_dir/"
-
-    # Restore previous scripts
-    if $preserve_previous_scripts; then
-        echo " > Restoring previous scripts to the install directory..."
-        mv "$tmp_install_dir" "$install_dir/User previous scripts"
-    fi
-
-    # Install 'scripts-accels'
-    if [[ -n "$accels_dir" ]]; then
-        echo " > Installing 'scripts-accels'..."
-        rm -f "$accels_dir/scripts-accels"
-        mkdir --parents "$accels_dir"
-        cp "scripts-accels" "$accels_dir/scripts-accels"
-    fi
-
-    # Set file permissions
-    echo " > Setting file permissions..."
-    find "$install_dir" -mindepth 2 -type f ! -path "*.git/*" -exec chmod +x {} \;
-
-    # Ask if the user wants to install some basic dependencies
-    read -r -p " > Would you like to install now some basic dependencies for the scripts? [Y/n]" answer
-    case "${answer,,}" in
-    y | yes | "")
-        echo " > Installing dependencies..."
-        _install_dependencies
-        ;;
-    esac
-
-    # Close the file manager
-    echo " > Closing the file manager to reload its configurations..."
-    eval "$file_manager -q &>/dev/null" || true
+    _install_scripts "$menu_options"
 
     echo "Done!"
 }
@@ -102,6 +50,8 @@ _command_exists() {
 
 _install_dependencies() {
     local common_names="bzip2 foremost ghostscript gzip jpegoptim lame lzip lzop optipng pandoc perl-base qpdf rhash squashfs-tools tar testdisk unrar wget xclip zip zstd"
+
+    echo " > Installing dependencies..."
 
     if _command_exists "sudo"; then
         if _command_exists "apt-get"; then
@@ -124,6 +74,71 @@ _install_dependencies() {
     else
         echo "Error: could not run as administrator!"
         exit 1
+    fi
+}
+
+_install_scripts() {
+    local menu_options="$1"
+    local accels_dir=""
+    local file_manager=""
+    local install_dir=""
+    local tmp_install_dir=""
+
+    # Get the default file manager
+    if _command_exists "nautilus"; then
+        install_dir="$HOME/.local/share/nautilus/scripts"
+        accels_dir="$HOME/.config/nautilus"
+        file_manager="nautilus"
+    elif _command_exists "nemo"; then
+        install_dir="$HOME/.local/share/nemo/scripts"
+        file_manager="nemo"
+    elif _command_exists "caja"; then
+        install_dir="$HOME/.config/caja/scripts"
+        file_manager="caja"
+    else
+        echo "Error: could not find any compatible file managers!"
+        exit 1
+    fi
+
+    # 'Preserve' or 'Remove' previous scripts
+    if [[ "$menu_options" == *3* ]]; then
+        echo " > Preserving previous scripts to a temporary directory..."
+        tmp_install_dir=$(mktemp -d)
+        mv "$install_dir"/* "$tmp_install_dir" || true
+    else
+        echo " > Removing previous scripts..."
+        rm -rf "$install_dir"
+    fi
+
+    # Install the scripts
+    echo " > Installing new scripts..."
+    mkdir --parents "$install_dir"
+    cp -r ./* "$install_dir/"
+
+    # Restore previous scripts
+    if [[ "$menu_options" == *3* ]]; then
+        echo " > Restoring previous scripts to the install directory..."
+        mv "$tmp_install_dir" "$install_dir/User previous scripts"
+    fi
+
+    # Install the file 'scripts-accels'
+    if [[ "$menu_options" == *2* ]]; then
+        if [[ -n "$accels_dir" ]]; then
+            echo " > Installing the file 'scripts-accels'..."
+            rm -f "$accels_dir/scripts-accels"
+            mkdir --parents "$accels_dir"
+            cp "scripts-accels" "$accels_dir/scripts-accels"
+        fi
+    fi
+
+    # Set file permissions
+    echo " > Setting file permissions..."
+    find "$install_dir" -mindepth 2 -type f ! -path "*.git/*" -exec chmod +x {} \;
+
+    # Close the file manager to reload its configurations
+    if [[ "$menu_options" == *4* ]]; then
+        echo " > Closing the file manager to reload its configurations..."
+        eval "$file_manager -q &>/dev/null" || true
     fi
 }
 
