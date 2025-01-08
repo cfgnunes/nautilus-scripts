b5eab55adb9f28801493a9c07b3f0b3bc28a938f
Cristiano Nunes <cfgnunes@gmail.com>
2025-01-07 21:03:53 -0300

    Revert "Allow 'maxdepth' configurable in the '_validate_file_preselect'"
    This reverts commit ca31c3fcca3e5b2e4ea77fa497fce119ee5c7b48.

--- common-functions.sh
+++ common-functions.sh
@@ -2172,10 +2172,10 @@ _validate_file_preselect() {
     #     included.
     #   - $4 (par_select_extension): A string of file extensions to include in
     #     the search. Only files with matching extensions will be included.
-    #   - $5 (par_recursive): Specifies the depth of recursion for the search:
-    #     - A numeric value: Limits the search to the specified maximum depth.
-    #     - "true": Enables full recursion with no depth limit.
-    #     - "false" (default): Limits the search to the current directory only.
+    #   - $5 (par_recursive): A boolean string ("true" or any other value)
+    #     indicating whether to search directories recursively. If set to
+    #     "true", directories will be searched recursively; otherwise, only the
+    #     immediate directory level will be searched.
     #
     # Example:
     #   - Input: "dir1 dir2", "file", "", "txt|pdf", "true"
@@ -2195,12 +2195,9 @@ _validate_file_preselect() {
     # Build a 'find' command.
     find_command="find '$input_files'"
 
-    # Parse the parameter $par_recursive.
-    case $par_recursive in
-    [[:digit:]]) find_command+=" -maxdepth $par_recursive" ;;
-    true) ;;
-    *) find_command+=" -maxdepth 0" ;;
-    esac
+    if [[ "$par_recursive" != "true" ]]; then
+        find_command+=" -maxdepth 0"
+    fi
 
     # Expand the directories with the 'find' command.
     case "$par_type" in
