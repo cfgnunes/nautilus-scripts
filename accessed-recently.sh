d873a3a14ab0c09ed1dc9c63955df03513c7dadf
Cristiano Nunes <cfgnunes@gmail.com>
2025-01-11 13:17:18 -0300

    Refactor

--- accessed-recently.sh
+++ accessed-recently.sh
@@ -4,17 +4,27 @@
 # It creates symbolic links to maintain a list of the 10 most recently accessed scripts.
 # The folder serves as a shortcut to quickly access frequently used scripts.
 
-ACCESSED_LINKS_PATH="$ROOT_DIR/Accessed recently"
-readonly ACCESSED_LINKS_PATH
+# -----------------------------------------------------------------------------
+# CONSTANTS
+# -----------------------------------------------------------------------------
+
+ACCESSED_RECENTLY_DIR="$ROOT_DIR/Accessed recently"
+NUM_LINKS_TO_KEEP=10
+
+readonly \
+    ACCESSED_RECENTLY_DIR \
+    NUM_LINKS_TO_KEEP
+
+# -----------------------------------------------------------------------------
+# FUNCTIONS
+# -----------------------------------------------------------------------------
 
 _clean_up_accessed_files() {
     # This function Cleans up the "Accessed recently" folder by:
     # - Removing duplicate symbolic links.
-    # - Keeping only the 10 most recently accessed files.
+    # - Keeping only the 'NUM_LINKS_TO_KEEP' most recently accessed files.
     # - Renaming the links with zero-padded numeric prefixes for easy sorting.
 
-    local directory="$ACCESSED_LINKS_PATH"
-    local num_files_to_keep=10
     local file=""
     declare -A file_map
 
@@ -33,17 +43,17 @@ _clean_up_accessed_files() {
             fi
             set -u
         fi
-    done < <(find "$directory" -maxdepth 1 -type l -printf '%T@ %p\0' | sort -r -z -n | cut -z -d " " -f2-)
+    done < <(find "$ACCESSED_RECENTLY_DIR" -maxdepth 1 -type l -printf '%T@ %p\0' | sort -r -z -n | cut -z -d " " -f2-)
 
-    # Delete all but the most recent 'num_files_to_keep' symbolic links.
-    for ((i = num_files_to_keep; i < ${#files[@]}; i++)); do
+    # Delete all but the most recent 'NUM_LINKS_TO_KEEP' symbolic links.
+    for ((i = NUM_LINKS_TO_KEEP; i < ${#files[@]}; i++)); do
         rm -f "${files[$i]}"
     done
 
     # Rename remaining files with zero-padded numeric prefixes for sorted display.
     local count=1
     for file in "${files[@]}"; do
-        mv "$file" "$directory/$(printf '%02d' $count) $(basename "$file" | sed 's/^\([0-9]\{2\} \)*//')"
+        mv "$file" "$ACCESSED_RECENTLY_DIR/$(printf '%02d' $count) $(basename "$file" | sed 's/^\([0-9]\{2\} \)*//')"
         ((count++))
     done
 }
@@ -56,7 +66,7 @@ _link_file_to_accessed() {
 
     local file="$1"
 
-    pushd "$ACCESSED_LINKS_PATH" &>/dev/null || return
+    pushd "$ACCESSED_RECENTLY_DIR" &>/dev/null || return
 
     # Create a symbolic link to the specified file in the directory.
     ln -s "$file" .
@@ -70,9 +80,14 @@ _update_accessed_recently_history() {
     local match_count=0
     local script_to_link_full_path=""
 
-    # Identify the script being executed to potentially add it to the "Accessed recently" folder.
+    # Ensure the '$ACCESSED_RECENTLY_DIR' directory exists, creating it if necessary.
+    if [[ ! -d $ACCESSED_RECENTLY_DIR ]]; then
+        mkdir -p "$ACCESSED_RECENTLY_DIR"
+    fi
+
+    # Identify the script being executed to potentially add it to the '$ACCESSED_RECENTLY_DIR' folder.
     script_name=$(basename -- "$(realpath -e "$0")")
-    script_matches=$(find "$ROOT_DIR" -path "$ACCESSED_LINKS_PATH" -prune -o -type f -name "$script_name" -print)
+    script_matches=$(find "$ROOT_DIR" -path "$ACCESSED_RECENTLY_DIR" -prune -o -type f -name "$script_name" -print)
 
     if [ -z "$script_matches" ]; then
         match_count=0
@@ -83,13 +98,6 @@ _update_accessed_recently_history() {
     # If exactly one match is found, store its full path for linking.
     if [[ $match_count == 1 ]]; then
         script_to_link_full_path="$script_matches"
-    else
-        _display_error_box "Can't find [ $script_name ] location to link in accessed"
-    fi
-
-    # Ensure the "Accessed recently" directory exists, creating it if necessary.
-    if [[ ! -d $ACCESSED_LINKS_PATH ]]; then
-        mkdir -p "$ACCESSED_LINKS_PATH"
     fi
 
     # If the script's full path is determined and it exists, link it to the folder.
