a887adcdac32843089e61783d598bbfb312fc7f7
Cristiano Nunes <cfgnunes@gmail.com>
2025-02-12 19:22:17 -0300

    fix: in 'accessed-recently.sh'

--- .helper-scripts/accessed-recently.sh
+++ .helper-scripts/accessed-recently.sh
@@ -42,7 +42,7 @@ _recent_scripts_organize() {
     for link in "${links[@]}"; do
         if ((count <= NUM_LINKS_TO_KEEP)); then
             # Rename the link with a numeric prefix for ordering.
-            mv -f -- "$link" "$ACCESSED_RECENTLY_DIR/$(printf '%02d' "$count") $(basename "$link" | sed --regexp-extended 's|^[0-9]{2} ||')" 2>/dev/null
+            mv -f -- "$link" "$ACCESSED_RECENTLY_DIR/$(printf '%02d' "$count") $(basename "$link" | sed --regexp-extended 's|^.?[0-9]{2} ||')" 2>/dev/null
             ((count++))
         else
             # Remove excess links.
@@ -78,8 +78,8 @@ _recent_scripts_add() {
     find "$ACCESSED_RECENTLY_DIR" -lname "$running_script" \
         -exec rm -f -- "{}" +
 
-    # Create a new symbolic link with a "00" prefix.
-    ln -s -- "$running_script" "00 $(basename -- "$running_script")"
+    # Create a new symbolic link with a ".00" prefix.
+    ln -s -- "$running_script" ".00 $(basename -- "$running_script")"
 
     _directory_pop || return 1
 }
