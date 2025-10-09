7d9db7fb212beda012f829af507c45d58b4f9141
Cristiano Nunes <cfgnunes@gmail.com>
2025-10-08 23:48:20 -0300

    fix: add ImageMagick post install

--- _dependencies.sh
+++ _dependencies.sh
@@ -271,6 +271,8 @@ declare -A POST_INSTALL=(
         *:sleep 5; rm -f /var/log/clamav/freshclam.log; \
             freshclam --quiet; sleep 5
     "
+
+    ["convert"]='*:find /etc -type f -path "/etc/ImageMagick-*/policy.xml" 2>/dev/null -exec sed -i -e "s/rights=\"none\" pattern=\"PDF\"/rights=\"read|write\" pattern=\"PDF\"/g" -e "s/name=\"disk\" value=\".GiB\"/name=\"disk\" value=\"8GiB\"/g" {} +'
 )
 
 # -----------------------------------------------------------------------------
