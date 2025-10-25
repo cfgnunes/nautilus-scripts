f0a492909332dc4b6f25233716e9f6a923beda36
Cristiano Nunes <cfgnunes@gmail.com>
2025-10-25 01:11:16 -0300

    chore: bump version

--- .common-functions.sh
+++ .common-functions.sh
@@ -8,7 +8,7 @@
 # AUTHOR: Cristiano Fraga G. Nunes
 # REPOSITORY: https://github.com/cfgnunes/nautilus-scripts
 # LICENSE: MIT License
-# VERSION: 25.3.2
+# VERSION: 25.4
 # =============================================================================
 
 set -u
