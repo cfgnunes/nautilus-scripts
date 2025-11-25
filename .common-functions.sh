fe9ce2f7097cebe9b8c2a334cab4e67200096e50
Cristiano Nunes <cfgnunes@gmail.com>
2025-11-25 04:14:18 -0300

    chore: bump version

--- .common-functions.sh
+++ .common-functions.sh
@@ -6,7 +6,7 @@
 # AUTHOR: Cristiano Fraga G. Nunes
 # REPOSITORY: https://github.com/cfgnunes/nautilus-scripts
 # LICENSE: MIT License
-# VERSION: 30.3
+# VERSION: 30.3.1
 # =============================================================================
 
 # This file contains shared functions and constants sourced by all scripts.
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # AUTHOR: Cristiano Fraga G. Nunes
 # REPOSITORY: https://github.com/cfgnunes/nautilus-scripts
 # LICENSE: MIT License
-# VERSION: 30.3
+# VERSION: 30.3.1
 # =============================================================================
 
 set -u
@@ -15,7 +15,7 @@ set -u
 # -----------------------------------------------------------------------------
 
 APP_NAME="Enhanced File Manager Actions for Linux"
-APP_VERSION="30.3"
+APP_VERSION="30.3.1"
 
 # Used in:
 #  - Directory where scripts are installed located at:
