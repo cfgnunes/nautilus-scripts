cdc88286ec065d2193f60182dead95af3bc2b30d
Cristiano Nunes <cfgnunes@gmail.com>
2025-11-26 13:00:15 -0300

    chore: bump version

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # AUTHOR: Cristiano Fraga G. Nunes
 # REPOSITORY: https://github.com/cfgnunes/nautilus-scripts
 # LICENSE: MIT License
-# VERSION: 30.4
+# VERSION: 30.5.1
 # =============================================================================
 
 set -u
@@ -15,7 +15,7 @@ set -u
 # -----------------------------------------------------------------------------
 
 APP_NAME="Enhanced File Manager Actions for Linux"
-APP_VERSION="30.4"
+APP_VERSION="30.5.1"
 
 # Used in:
 #  - Directory where scripts are installed located at:
