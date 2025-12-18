53ae72a1cc141a48724c59ced2af0553a2b3bfeb
Cristiano Nunes <cfgnunes@gmail.com>
2025-12-18 11:13:01 -0300

    chore: bump version

--- .common-functions.sh
+++ .common-functions.sh
@@ -6,7 +6,7 @@
 # Author: Cristiano Fraga G. Nunes
 # Repository: https://github.com/cfgnunes/fm-scripts
 # License: MIT License
-# Version: 30.12.2
+# Version: 30.13
 # =============================================================================
 
 # This file contains functions and constants sourced by scripts.
