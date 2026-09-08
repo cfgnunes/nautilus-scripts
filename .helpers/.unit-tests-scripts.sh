d4e320a743aed500fccee846da198758f332264d
Cristiano Nunes <cfgnunes@gmail.com>
2026-09-08 16:15:42 -0300

    chore: remove unnecessary scripts

--- .helpers/.unit-tests-scripts.sh
+++ .helpers/.unit-tests-scripts.sh
@@ -996,12 +996,6 @@ _main() {
     __test_file_nonempty "$output_file (icon 512 px).png"
     __test_file_empty "$std_output"
 
-    script_test="Image/Image: Icons/Image: Create PNG icon (64 px)"
-    __echo_script "$script_test"
-    bash "$ROOT_DIR/$script_test" "$input_file1" >"$std_output"
-    __test_file_nonempty "$output_file (icon 64 px).png"
-    __test_file_empty "$std_output"
-
     script_test="Image/Image: Optimize, Reduce/Image: Optimize PNG"
     __echo_script "$script_test"
     bash "$ROOT_DIR/$script_test" "$input_file1" >"$std_output"
